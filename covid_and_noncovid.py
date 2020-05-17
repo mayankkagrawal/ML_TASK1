8f5ec8542fa8ffbdada8a52421d2826daf159222
mayankkagrawal <ma.agrawal123@gmail.com>
2020-05-17 18:36:57 +0530

    wdwd

--- covid_and_noncovid.py
+++ covid_and_noncovid.py
@@ -1,6 +1,14 @@
 
 
+
+
 import pandas as pd
+
+
+
+
+
+
 
 
 
@@ -10,11 +18,27 @@ from keras.layers import Convolution2D
 
 
 
+
+
+
+
 from 
 
 
 
+
+
+
+
 keras.layers import MaxPooling2D
+
+
+
+
+
+
+
+
 
 
 
@@ -30,7 +54,23 @@ from keras.layers import Flatten
 
 
 
+
+
+
+
+
+
+
+
 from keras.layers import Dense
+
+
+
+
+
+
+
+
 
 
 
@@ -46,7 +86,23 @@ from keras.models import Sequential
 
 
 
+
+
+
+
+
+
+
+
 model=Sequential()
+
+
+
+
+
+
+
+
 
 
 
@@ -56,11 +112,19 @@ model=Sequential()
 
 model.add(Convolution2D(filters=32,
 
+
+
 kernel_size=(3,3),
+
+
 
 activation='relu',
 
+
+
 input_shape=(64,64,3)
+
+
 
 ))
 
@@ -70,7 +134,23 @@ input_shape=(64,64,3)
 
 
 
+
+
+
+
+
+
+
+
 model.summary()
+
+
+
+
+
+
+
+
 
 
 
@@ -81,6 +161,17 @@ model.summary()
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64,64,3)))
  model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64,64,3)))
+
+ model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64,64,3)))
+ model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+
+
+
+
 
 
 
@@ -88,6 +179,14 @@ model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_sha
 
 
 model.summary()
+
+
+
+
+
+
+
+
 
 
 
@@ -103,7 +202,23 @@ model.add(Flatten())
 
 
 
+
+
+
+
+
+
+
+
 model.summary()
+
+
+
+
+
+
+
+
 
 
 
@@ -119,7 +234,25 @@ model.add(Dense(units=128,activation='relu'))
 
 
 
+
+
+
+
+
+
+
+
 model.summary()
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -137,7 +270,23 @@ model.add(Dense(units=1,activation='sigmoid'))
 
 
 
+
+
+
+
+
+
+
+
 model.summary()
+
+
+
+
+
+
+
+
 
 
 
@@ -153,7 +302,23 @@ from keras.optimizers import Adam
 
 
 
+
+
+
+
+
+
+
+
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+
+
+
+
+
+
+
 
 
 
@@ -169,55 +334,115 @@ from keras_preprocessing.image import ImageDataGenerator
 
 
 
+
+
+
+
+
+
+
+
 train_datagen = ImageDataGenerator(
+
+
 
         rescale=1./255,
 
+
+
         shear_range=0.2,
+
+
 
         zoom_range=0.2,
 
+
+
         horizontal_flip=True)
+
+
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 
 
+
+
+
+
 training_set = train_datagen.flow_from_directory(
+
+
 
         'cnn_covid/training_set',
 
+
+
         target_size=(64, 64),
+
+
 
         batch_size=32,
 
+
+
         class_mode='binary')
+
+
+
+
 
 
 
 test_set = test_datagen.flow_from_directory(
 
+
+
         'cnn_covid/test_set',
+
+
 
         target_size=(64, 64),
 
+
+
         batch_size=32,
+
+
 
         class_mode='binary')
 
 
 
+
+
+
+
 model.fit(
+
+
 
         training_set,
 
+
+
         steps_per_epoch=616,
+
+
 
         epochs=5,
 
+
+
         validation_data=test_set,
 
+
+
         validation_steps=800)
+
+
+
+
 
 
 
@@ -225,7 +450,15 @@ model.save('cnn_cat_dog.h5')
 
 
 
+
+
+
+
 df_loss=pd.DataFrame(model.history.history)
+
+
+
+
 
 
 
@@ -233,7 +466,15 @@ accuracy=df_loss.iloc[615,0]
 
 
 
+
+
+
+
 print(accuracy)
+
+
+
+
 
 
 
