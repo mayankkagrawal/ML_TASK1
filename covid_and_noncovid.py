3f93fe7bcb430788956a70c7dfd0f3c004919bea
mayankkagrawal <ma.agrawal123@gmail.com>
2020-05-17 19:18:08 +0530

    qdqd

--- covid_and_noncovid.py
+++ covid_and_noncovid.py
@@ -30,12 +30,12 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'cnn_covid/training_set',
+        '/root/db/cnn_covid/training_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'cnn_covid/test_set',
+        '/root/db/cnn_covid/test_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
