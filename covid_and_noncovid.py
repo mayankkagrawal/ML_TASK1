aa0ee16c06bddbfc4f5fc482a29e237ad4ed7e70
mayankkagrawal <ma.agrawal123@gmail.com>
2020-05-17 10:15:27 +0530

    new

--- covid_and_noncovid.py
+++ covid_and_noncovid.py
@@ -158,9 +158,7 @@ model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_sha
 
 
 
-m odel.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64,64,3)))
- model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(2,2)))
 #model.summary()
 
 
